4b9845daa27e4bfccfc5403defc68d7cbc548c80
Osvald Lindholm <osvald.lindholm@gmail.com>
2022-10-01 11:36:01 +0200

    Add tests for pysh.shell.sh

---
+++ tests/pysh/shell/test_sh.py
@@ -0,0 +1,35 @@
+import inspect
+from unittest.mock import MagicMock, call, patch
+
+from pysh import asyncshell
+from pysh.shell import ProcessInfo, sh
+
+
+def test_signature() -> None:
+    assert inspect.signature(sh) == inspect.signature(asyncshell.sh)
+
+
+@patch("pysh.shell.asyncshell.sh")
+def test_kwargs(asyncshell_sh_mock: MagicMock) -> None:
+    # Arrange
+    params = {
+        param: index for index, param in enumerate(inspect.signature(sh).parameters)
+    }
+
+    # Act
+    sh(**params)
+
+    # Assert
+    assert asyncshell_sh_mock.call_args_list == [call(**params)]
+
+
+@patch("pysh.shell.asyncshell.sh")
+def test_return_value(asyncshell_sh_mock: MagicMock) -> None:
+    # Arrange
+    asyncshell_sh_mock.return_value = ProcessInfo("exitcode", "stdout", "stderr")
+
+    # Act
+    process_info = sh([])
+
+    # Assert
+    assert process_info == ProcessInfo("exitcode", "stdout", "stderr")
