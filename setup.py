c25239f01ef8216be1a336891559d78489d89f9e
Osvald Lindholm <osvald.lindholm@gmail.com>
2022-11-24 17:35:55 +0100

    Fix flake8

--- setup.py
+++ setup.py
@@ -1,10 +1,39 @@
+import logging
+import os
+
+from packaging.version import Version
 from setuptools import setup
+
+
+def get_env_var(env_var: str, default: str) -> str:
+    value = os.getenv(env_var)
+
+    if not value:
+        logging.warning(
+            f"Environment variable, ({env_var}), was not set. "
+            f"Defaulting to '{default}'."
+        )
+        return default
+
+    return value
+
+
+def get_version() -> Version:
+    version = get_env_var("PACKAGE_VERSION", "0+dev")
+    build = int(get_env_var("BUILD_NUMBER", "1")) - 1
+
+    if build > 0:
+        version = f"{version}.{build}"
+
+    return Version(version)
+
 
 deps = {
     "",
 }
 test_deps = {
     "pytest>=7",
+    "pytest-asyncio",
     "pytest-cov",
 }
 typing_deps = {
@@ -18,12 +47,12 @@ dev_deps = {
     "isort",
     *typing_deps,
     "flake8",
-    "flake8-pyproject",
+    "flake8-pyproject>=1.2.0",
 }
 
 setup(
     name="pysh",
-    version="0+dev",
+    version=str(get_version()),
     python_requires=">=3.8",
     install_requires=[deps],
     extras_require={
